2434dcb3063a8e9a20075ee27a425e3bf8ae80e4
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-23 17:26:18 -0600

    AvionRepository

--- aerolinea/src/main/java/org/una/aerolinea/repositories/IAvionRepository.java
+++ aerolinea/src/main/java/org/una/aerolinea/repositories/IAvionRepository.java
@@ -7,6 +7,8 @@ package org.una.aerolinea.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.una.aerolinea.entities.Avion;
 
 /**
@@ -17,7 +19,11 @@ public interface IAvionRepository extends JpaRepository<Avion, Long>{
     public List<Avion> findByMatricula(String matricula);
     public List<Avion> findByMatriculaContainingIgnoreCase(String matricula);
     public List<Avion> findByEstado(boolean estado);
-    public List<Avion> findByAerolinea(Long aerolinea);
-    public List<Avion> findByTipoAvion(Long tipoAvion);
+    
+    @Query("SELECT a FROM Avion a LEFT JOIN a.aerolinea ae WHERE ae.id = :aerolineaID")
+    public List<Avion> findByAerolinea(@Param("aerolineaID")Long aerolinea);
+    
+    @Query("SELECT a FROM Avion a LEFT JOIN a.tipoAvion t WHERE t.id = :tipoAvionID")
+    public List<Avion> findByTipoAvion(@Param("tipoAvionID") Long tipoAvion);
     
 }
