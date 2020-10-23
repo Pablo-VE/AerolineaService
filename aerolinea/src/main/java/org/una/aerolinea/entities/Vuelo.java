29b111e576f85a6190baeed0b3fb62578240eb56
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-23 17:01:09 -0600

    Vuelo repository

--- aerolinea/src/main/java/org/una/aerolinea/entities/Vuelo.java
+++ aerolinea/src/main/java/org/una/aerolinea/entities/Vuelo.java
@@ -18,7 +18,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
